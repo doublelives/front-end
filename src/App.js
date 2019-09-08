7c803b19c38f8f2f15fa1ff15c6fe9bc21bb4eb0
MR7464 <t15151414@163.com>
2019-09-08 21:17:46 +0800

    set route config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import routeConfig from './config/routes'
+import { Switch, Route, withRouter, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Link to="/Home">Home</Link>
+    <Link to="/User">User</Link>
+    <Switch>
+      {
+        routeConfig.map((route, index) => {
+          return <Route key = {index} {...route} />
+        })
+      }
+    </Switch>
+    </>
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/config/routes.js
@@ -0,0 +1,6 @@
+import Home from '../pages/home/home'
+import User from '../pages/user/user'
+export default [
+  { path: '/Home', component: Home },
+  { path: '/User', component: User }
+]
