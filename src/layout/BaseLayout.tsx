56a521ccd23cae60582236bae1c6978acb9c8a23
平超 <chao.ping@daddylab.com>
2020-10-25 15:25:04 +0800

    feat: 加入ts配置

---
+++ src/layout/BaseLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BaseLayout() {
+  return <div>
+    <header>我是头</header>
+    <aside>我是侧边栏</aside>
+    <main>我是主体</main>
+  </div>
+}
+
+export default BaseLayout
